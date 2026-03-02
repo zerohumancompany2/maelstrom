6a9ef9d052359dac1832b87e34f38494d48efcae
Albert Latham <me@albertlatham.com>
2026-03-02 02:18:09 +0000

    feat: heartbeat service with cron storage

--- pkg/services/heartbeat/service.go
+++ pkg/services/heartbeat/service.go
@@ -1,16 +1,31 @@
 package heartbeat
 
-import "errors"
+import (
+	"errors"
+	"sync"
+)
 
 var NotImplementedError = errors.New("not implemented")
 
-type heartbeatService struct{}
+type heartbeatService struct {
+	schedules map[string]Schedule
+	mu        sync.Mutex
+}
 
 func NewHeartbeatService() HeartbeatService {
-	return &heartbeatService{}
+	return &heartbeatService{
+		schedules: make(map[string]Schedule),
+	}
 }
 
 func (s *heartbeatService) Schedule(agentId string, cron string, template string) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.schedules[agentId] = Schedule{
+		AgentID:  agentId,
+		CronExpr: cron,
+		Template: template,
+	}
 	return nil
 }
 
@@ -23,5 +38,7 @@ func (s *heartbeatService) Trigger(agentId string) error {
 }
 
 func (s *heartbeatService) GetSchedule(agentId string) (Schedule, error) {
-	return Schedule{}, nil
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return s.schedules[agentId], nil
 }
