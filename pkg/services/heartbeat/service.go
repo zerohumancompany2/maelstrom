7cadf2e4d7910b10060f5e4d8a7c501c15e7414a
Albert Latham <me@albertlatham.com>
2026-03-02 02:17:12 +0000

    feat: heartbeat service with Schedule capability

---
+++ pkg/services/heartbeat/service.go
@@ -0,0 +1,27 @@
+package heartbeat
+
+import "errors"
+
+var NotImplementedError = errors.New("not implemented")
+
+type heartbeatService struct{}
+
+func NewHeartbeatService() HeartbeatService {
+	return &heartbeatService{}
+}
+
+func (s *heartbeatService) Schedule(agentId string, cron string, template string) error {
+	return nil
+}
+
+func (s *heartbeatService) Unschedule(agentId string) error {
+	return NotImplementedError
+}
+
+func (s *heartbeatService) Trigger(agentId string) error {
+	return NotImplementedError
+}
+
+func (s *heartbeatService) GetSchedule(agentId string) (Schedule, error) {
+	return Schedule{}, NotImplementedError
+}
