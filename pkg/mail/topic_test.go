d61c864b11976f601c57ecf6f8a177491be47a2f
Albert Latham <me@albertlatham.com>
2026-03-08 23:31:16 -0700

    feat: add Topic Publish broadcast method

--- pkg/mail/topic_test.go
+++ pkg/mail/topic_test.go
@@ -40,3 +40,32 @@ func TestTopic_SubscribeUnsubscribe(t *testing.T) {
 	}
 	topic.mu.RUnlock()
 }
+
+func TestTopic_Broadcast(t *testing.T) {
+	topic := &Topic{Name: "market-data"}
+
+	sub1 := &mockSubscriber{ch: make(chan Mail, 10)}
+	sub2 := &mockSubscriber{ch: make(chan Mail, 10)}
+	sub3 := &mockSubscriber{ch: make(chan Mail, 10)}
+
+	topic.Subscribe(sub1)
+	topic.Subscribe(sub2)
+	topic.Subscribe(sub3)
+
+	mail := Mail{ID: "msg-001", Type: MailTypeAssistant}
+	err := topic.Publish(mail)
+	if err != nil {
+		t.Errorf("Expected nil error, got %v", err)
+	}
+
+	for i, sub := range []*mockSubscriber{sub1, sub2, sub3} {
+		select {
+		case received := <-sub.ch:
+			if received.ID != "msg-001" {
+				t.Errorf("Subscriber %d: Expected msg-001, got %s", i, received.ID)
+			}
+		default:
+			t.Errorf("Subscriber %d: Expected to receive message", i)
+		}
+	}
+}
