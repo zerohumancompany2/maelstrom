029f30f91402a0364413e89ad5d8be51ae956e26
Albert Latham <me@albertlatham.com>
2026-03-09 17:24:28 -0700

    feat(layer-4/human-gateway): stub sanitization functions for human-gateway

---
+++ pkg/security/human_gateway.go
@@ -0,0 +1,11 @@
+package security
+
+import "github.com/maelstrom/v3/pkg/mail"
+
+func SanitizeContextMap(ctx ContextMap, boundary BoundaryType) (ContextMap, error) {
+	panic("not implemented")
+}
+
+func SanitizeMessageHistory(messages []mail.Mail, boundary BoundaryType) ([]mail.Mail, error) {
+	panic("not implemented")
+}
---
+++ pkg/security/human_gateway_test.go
@@ -0,0 +1,15 @@
+package security
+
+import (
+	"testing"
+
+	"github.com/maelstrom/v3/pkg/mail"
+)
+
+func TestHumanGatewaySanitization_SanitizeContextMap_StripsForbiddenTaints(t *testing.T) {
+	panic("not implemented")
+}
+
+func TestHumanGatewaySanitization_SanitizeMessageHistory_BoundaryEnforcement(t *testing.T) {
+	panic("not implemented")
+}
