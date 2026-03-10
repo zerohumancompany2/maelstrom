3a8eb34ddee62883c352d50f6d0dc2af578584f6
Albert Latham <me@albertlatham.com>
2026-03-10 11:32:27 -0700

    fix(layer-4/security): restore RedactMode check in CheckTaintPolicy

--- pkg/services/security/service.go
+++ pkg/services/security/service.go
@@ -333,15 +333,17 @@ func (s *SecurityService) CheckTaintPolicy(data any, targetBoundary mail.Boundar
 
 	taints := s.extractTaints(data)
 
-	for _, taint := range taints {
-		if taint == "INNER_ONLY" || taint == "SECRET" {
-			if targetBoundary != mail.InnerBoundary {
-				return false, nil
+	if policy.RedactMode == "strict" {
+		for _, taint := range taints {
+			if taint == "INNER_ONLY" || taint == "SECRET" {
+				if targetBoundary != mail.InnerBoundary {
+					return false, nil
+				}
 			}
-		}
-		if taint == "PII" {
-			if targetBoundary == mail.OuterBoundary {
-				return false, nil
+			if taint == "PII" {
+				if targetBoundary == mail.OuterBoundary {
+					return false, nil
+				}
 			}
 		}
 	}
